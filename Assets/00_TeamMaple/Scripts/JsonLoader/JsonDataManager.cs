9804d2fba62e3b11fc2bd09ed1eb41d98d5d0485
TaeGun08 <gustn4984@naver.com>
2025-07-08 17:01:49 +0900

    Firebase 제외 하고 Json 데이터 저장 불러오기로 변경하였음

---
+++ Assets/00_TeamMaple/Scripts/JsonLoader/JsonDataManager.cs
@@ -0,0 +1,26 @@
+using System.IO;
+using Newtonsoft.Json;
+using UnityEngine;
+
+public class JsonDataManager : SingletonBehaviour<JsonDataManager>
+{
+    public void SaveServerData(Server_Data data)
+    {
+        string jsonData = JsonConvert.SerializeObject(data);
+        PlayerPrefs.SetString("PlayerData", jsonData);
+        string filePath = Path.Combine(Application.persistentDataPath, "PlayerData.json");
+        File.WriteAllText(filePath, jsonData);
+        Base_Mng.Firebase.WriteData(PlayerPrefs.GetString("PlayerData"), jsonData);
+    }
+
+    public Server_Data LoadServerData()
+    {
+        string readPath = Path.Combine(Application.persistentDataPath, "PlayerData.json");
+        string rawJson = File.ReadAllText(readPath);
+
+        // ? 역직렬화: JSON → Server_Data 객체
+        Server_Data data = JsonConvert.DeserializeObject<Server_Data>(rawJson);
+        
+        return data;
+    }
+}
