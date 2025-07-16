9d7c2bfb36477eef9ff886eb82d633b7fb367927
TaeGun08 <gustn4984@naver.com>
2025-07-16 17:20:39 +0900

    스테이지 달성 별 보상 추가 중

--- Assets/00_TeamMaple/Scripts/Manager/StageManager.cs
+++ Assets/00_TeamMaple/Scripts/Manager/StageManager.cs
@@ -1,25 +1,85 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json; // Ãß°¡
 using UnityEngine;
 
-public class StageManager : MonoBehaviour
+[System.Serializable]
+public class StageClearData
+{
+    public int stageId;
+    public int stars; // È¹µæÇÑ º° °³¼ö (0~3)
+}
+
+[System.Serializable]
+public class StageSaveData
+{
+    public List<StageClearData> stages = new();
+}
+
+public class StageManager : SingletonBehaviour<StageManager>
 {
     public static StageManager Instance { get; private set; }
-
+    
+    private string SavePath => Path.Combine(Application.persistentDataPath, "stage_save.json");
+    public StageSaveData CurrentSaveData { get; private set; }
+    
     [Header("StageSO")] 
     [SerializeField] private ProceduralMapGeneratorSO so;
     public StageData StageData { get; private set; }
     public StageRewardData StageRewardData { get; private set; }
-    
+
     private void Awake()
     {
         Instance = this;
-        
+
         StageData = new StageData();
         StageRewardData = new StageRewardData();
-        
+
         so.InitData();
-        
         StageData = StageLoader.SetStageData(so);
         StageRewardData = so.GetStageRewardData(JsonDataLoader.LoadServerData().CurrentStage + 1000);
+
+        Load();
+    }
+
+    public void Load()
+    {
+        if (File.Exists(SavePath))
+        {
+            string json = File.ReadAllText(SavePath);
+            CurrentSaveData = JsonConvert.DeserializeObject<StageSaveData>(json);
+        }
+        else
+        {
+            CurrentSaveData = new StageSaveData();
+        }
+    }
+
+    public void Save()
+    {
+        string json = JsonConvert.SerializeObject(CurrentSaveData, Formatting.Indented);
+        File.WriteAllText(SavePath, json);
+    }
+
+    public void UpdateStageStars(int stageId, int newStars)
+    {
+        var stageData = CurrentSaveData.stages.FirstOrDefault(s => s.stageId == stageId);
+        if (stageData != null)
+        {
+            if (newStars > stageData.stars)
+                stageData.stars = newStars;
+        }
+        else
+        {
+            CurrentSaveData.stages.Add(new StageClearData { stageId = stageId, stars = newStars });
+        }
+        Save();
+    }
+
+    public int GetStars(int stageId)
+    {
+        return CurrentSaveData.stages.FirstOrDefault(s => s.stageId == stageId)?.stars ?? 0;
     }
 }
